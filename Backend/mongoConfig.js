758ac43d4112311404fb8b9732eb0fa05448d23e
Gamerk786po <abdullahpper@gmail.com>
2025-04-22 10:32:23 +0500

    server.js and mongoConfig updated

--- Backend/mongoConfig.js
+++ Backend/mongoConfig.js
@@ -2,10 +2,14 @@ const mongoose = require("mongoose");
 // connection funciton
 mongoose.connect("mongodb://localhost:27017/FavAnimeCharacters");
 //  Schema
-const infoSchema = new mongoose.Schema({
-  name: String,
-  url: String,
-  image: String,
-},{ collection: 'Informations' });
+const infoSchema = new mongoose.Schema(
+  {
+    mal_id: Number,
+    name: String,
+    url: String,
+    image: String,
+  },
+  { collection: "Informations" }
+);
 const Anime = new mongoose.model("Informations", infoSchema);
-module.exports = Anime
+module.exports = Anime;
---
+++ Backend/server.js
@@ -0,0 +1,40 @@
+//Importing
+const express = require("express");
+const Anime = require("./mongoConfig");
+
+// setting an instance for express
+const app = express();
+
+// middleware
+app.use(express.json()); // parser of json
+
+// Testing route
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+// route for put req
+app.put("/putAnime", async (req, res) => {
+  const { mal_id, name, url, image } = req.body;
+
+  try {
+    const updateCreateAnime = await Anime.findOneAndUpdate(
+      { mal_id },
+      {name, url, image },
+      { new: true, upsert: true }
+    );
+
+    // Send response back
+    res.status(200).json({
+      message: "Anime saved/updated successfully",
+      data: updateCreateAnime,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Error updating data", error });
+  }
+});
+
+// listening port
+app.listen(4000, () => {
+  console.log("Server is running on port 4000");
+});
