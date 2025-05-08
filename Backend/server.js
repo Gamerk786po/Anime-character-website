2caf9006b600d53142bf1633a4bab3dc861c35e3
Gamerk786po <abdullahpper@gmail.com>
2025-05-09 03:36:51 +0500

    port edited

--- Backend/server.js
+++ Backend/server.js
@@ -72,7 +72,6 @@ app.delete("/delAnime", async (req, res) => {
 });
 
 // listening port
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(4000, () => {
+  console.log("Server is running on port 4000");
 });
